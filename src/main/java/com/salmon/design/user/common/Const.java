b54224aa4e1c50dae68f9946bb3f3644c049443d
salmon1802 <757635525@qq.com>
2021-05-31 21:39:21 +0800

    基于springboot的user表的crud基本操作 (软件设计课程设计)

--- src/main/java/com/salmon/design/user/common/Const.java
+++ src/main/java/com/salmon/design/user/common/Const.java
@@ -18,43 +18,13 @@ public class Const {
 
     public static final String EMAIL = "email";
 
-    public interface  ActivityListOrderBy{
-        Set<String> PEOPLE_ASC_DESC = Sets.newHashSet("people_desc","people_asc");
-    }
 
-    public interface MyActivity{
-        int CHECKED = 1;//即活动选中状态
-        int UN_CHECKED = 0;//活动未选中状态
-
-        String LIMIT_NUM_FAIL = "LIMIT_NUM_FAIL";
-        String LIMIT_NUM_SUCCESS = "LIMIT_NUM_SUCCESS";
-    }
 
     public interface Role {
         String ROLE_CUSTOMER = "普通用户"; //普通用户
         String ROLE_ADMIN = "管理员";  //管理员
     }
 
-    public enum ActivityStatusEnum{
-
-
-        ON_PROCEED(1,"进行");
-
-        private String value;
-        private int code;
-        ActivityStatusEnum(int code,String value){
-            this.code = code;
-            this.value = value;
-        }
-
-        public String getValue() {
-            return value;
-        }
-
-        public int getCode() {
-            return code;
-        }
-    }
 
 
 
